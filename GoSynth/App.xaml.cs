ece27e5b8474a613d2d91de823bca93c23c9fbb6
Nathan Ernst <nathan.ernst@gmail.com>
2023-05-21 13:56:59 -0700

    Work on beat loops

--- GoSynth/App.xaml.cs
+++ GoSynth/App.xaml.cs
@@ -1,12 +1,27 @@
-﻿namespace GoSynth
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿ 
+namespace GoSynth;
 
-            MainPage = new AppShell();
-        }
+public partial class App : Application
+{
+    const int WindowDefaultWidth = 1080;
+    const int WindowDefaultHeight = 800;
+
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+           
     }
+
+#if WINDOWS
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        window.Width = WindowDefaultWidth;
+        window.Height = WindowDefaultHeight;
+
+        return window;
+    }
+#endif
 }
